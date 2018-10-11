a415913c6cb04690e0c71452ee715b58d20635f6
Mthuli Bhebhe <wildlyclassyprince@users.noreply.github.com>
2018-10-11 11:26:30 +0200

    Added comments

--- flaskr/create_db.py
+++ flaskr/create_db.py
@@ -1,12 +1,11 @@
 # create_db.py
 
-
+# Imports
 from app import db
 from models import Flaskr
 
-
-# create the database and the db table
+# Create the database and the db table
 db.create_all()
 
-# commit the changes
+# Commit the changes
 db.session.commit()
